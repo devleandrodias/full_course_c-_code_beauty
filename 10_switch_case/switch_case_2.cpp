dc6d0908f96fc3f57a12e2c97117143d09d5d749
devleandrodias <leandrodbdias@gmail.com>
2021-08-23 13:11:52 -0300

    finish program check number of days

---
+++ 10_switch_case/switch_case_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  system("clear");
+
+  int year, month;
+  cout << "Year, month: ";
+  cin >> year >> month;
+
+  switch (month)
+  {
+  case 2:
+    (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? cout << "29 days" << endl : cout << "28 days" << endl;
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    cout << "30 days month" << endl;
+    break;
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    cout << "31 days month" << endl;
+  }
+
+  return 0;
+}
