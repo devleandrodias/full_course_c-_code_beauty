43e95aca559e323c42a99ade3cc48a6eeddfeb2c
devleandrodias <leandrodbdias@gmail.com>
2021-08-24 18:02:18 -0300

    finish triangle shape

--- 15_shapes/triangle_shape.cpp
+++ 15_shapes/triangle_shape.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
   system("clear");
+
+  int base;
+  cout << "Base triangle: ";
+  cin >> base;
+
+  char symbol;
+  cout << "Symbol: ";
+  cin >> symbol;
+
+  system("clear");
+
+  for (int x = 0; x <= base; x++)
+  {
+    for (int y = 0; y < x; y++)
+    {
+      cout << setw(3) << symbol;
+    }
+
+    cout << endl;
+  }
 
   return 0;
 }
